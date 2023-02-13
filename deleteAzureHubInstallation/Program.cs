a3a7cd2b6f086ed42fdd7d71153fd46f1eca5edc
ololadeoyebanji <loladeking@hotmail.com>
2023-02-13 23:30:18 +0000

    wip

--- deleteAzureHubInstallation/Program.cs
+++ deleteAzureHubInstallation/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace deleteAzureHubInstallation;
+﻿using System.Net.Http.Json;
+
+namespace deleteAzureHubInstallation;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        HttpClient httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("apiKey", "Technololy22086Azure");
+        DeleteInstallations(httpClient);
+
+    }
+
+    private static async void DeleteInstallations(HttpClient httpClient)
+    {
+        var result =  httpClient.GetFromJsonAsync<List<Pushnotif>>("https://pushnoticationapi.azurewebsites.net/api/Notifications/GetAllRegisteredDevices").Result;
+        for (int i = 0; i < result?.Count; i++)
+        {
+            Pushnotif? item = result[i];
+            var installationId = item.Tags[0];
+            int pFrom = installationId.IndexOf("{")+1;
+            int pTo = installationId.LastIndexOf("}");
+
+            String result2 = installationId.Substring(pFrom, pTo - pFrom);
+            var url = $"https://pushnoticationapi.azurewebsites.net/api/Notifications/installations/{result2}";
+            Console.WriteLine(url);
+            var response = httpClient.DeleteAsync(url).Result;
+        }
     }
 }
 
---
+++ deleteAzureHubInstallation/pushmodel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+namespace deleteAzureHubInstallation
+{
+    public partial class Pushnotif
+    {
+        [JsonPropertyName("eTag")]
+        //[JsonConverter(typeof(ParseStringConverter))]
+        public long ETag { get; set; }
+
+        [JsonPropertyName("expirationTime")]
+        public DateTimeOffset ExpirationTime { get; set; }
+
+        [JsonPropertyName("registrationId")]
+        public string RegistrationId { get; set; }
+
+        [JsonPropertyName("tags")]
+        public List<string> Tags { get; set; }
+
+        [JsonPropertyName("pushVariables")]
+        public object PushVariables { get; set; }
+
+        [JsonPropertyName("pnsHandle")]
+        public string PnsHandle { get; set; }
+
+        [JsonPropertyName("isReadOnly")]
+        public bool IsReadOnly { get; set; }
+
+        [JsonPropertyName("extensionData")]
+        public ExtensionData ExtensionData { get; set; }
+    }
+
+    public partial class ExtensionData
+    {
+    }
+}
+
+
